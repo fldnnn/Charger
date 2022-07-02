28a3dfdc9d064a7724894396923aa0b0781dcaa9
fldnnn <fuldenonan@gmail.com>
2022-07-02 16:06:07 +0300

    initial commit

---
+++ charger_app/charger_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  charger_app
+//
+//  Created by Fulden Onan on 2.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
