87c504928a22100645a5c78004f923bdb29ef6e6
fldnnn <fuldenonan@gmail.com>
2022-07-10 19:46:14 +0300

    Station Selection Cell Screen Implementation
    - Station selection cell screen implementations started

---
+++ charger_app/charger_app/Modules/StationSelection/StationSelecetionTableViewAdapter/Cell/StationTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StationTableViewCell.swift
+//  charger_app
+//
+//  Created by Fulden Onan on 10.07.2022.
+//
+
+import UIKit
+
+class StationTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var socketNumberLabel: UILabel!
+    @IBOutlet private weak var distanceLabel: UILabel!
+    @IBOutlet private weak var stationNameLabel: UILabel!
+    @IBOutlet private weak var stationTypeImageView: UIImageView!
+}
