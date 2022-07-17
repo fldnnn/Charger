9e1c904897ae70213a2da5288e95c4c1d3c58e5f
fldnnn <fuldenonan@gmail.com>
2022-07-17 18:31:38 +0300

    Station Filter Selection Screen Implementation
    - Launch screen display time is changed

--- charger_app/charger_app/Application/SceneDelegate.swift
+++ charger_app/charger_app/Application/SceneDelegate.swift
@@ -7,25 +7,23 @@
 
 import UIKit
 
-//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
 //
-//    var window: UIWindow?
 //
-//
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-//        guard let _ = (scene as? UIWindowScene) else { return }
-//        
-//        // setting the Login module's view controller as the root view controller.
-//        if let windowScene = scene as? UIWindowScene {
-//            let window = UIWindow(windowScene: windowScene)
-//            window.rootViewController = Login.createModule()
-//            self.window = window
-//            window.makeKeyAndVisible()
-//        }
-//    }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+            window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+            window?.makeKeyAndVisible()
+
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+                self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+            }
+
+            guard let _ = (scene as? UIWindowScene) else { return }
+        }
+    }
 //    func sceneDidDisconnect(_ scene: UIScene) {
 //        // Called as the scene is being released by the system.
 //        // This occurs shortly after the scene enters the background, or when its session is discarded.
